7db728b10963444c9dcfa75182f0dffbe0c39c4f
M Fajar Mukti <fajar.mukti@mceasy.co.id>
2025-07-21 12:08:25 +0700

    chg list view pagination without controller

--- lib/presentations/components/layout/list_view.dart
+++ lib/presentations/components/layout/list_view.dart
@@ -1,49 +1,106 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 export 'package:pull_to_refresh/pull_to_refresh.dart';
 
-class XListView extends StatelessWidget {
-  final RefreshController refreshController;
-  final VoidCallback? onRefresh;
-  final VoidCallback? onLoading;
-  final int itemsCount;
-  final NullableIndexedWidgetBuilder itemBuilder;
-  final Widget? onEmptyDisplay;
-  final bool? isEmpty;
-
+class XListView extends StatefulWidget {
   const XListView({
-    super.key,
-    required this.refreshController,
-    this.onRefresh,
-    this.onLoading,
     required this.itemsCount,
     required this.itemBuilder,
+    super.key,
+    this.initialRefresh = true,
+    this.onRefresh,
+    this.onLoading,
+    this.itemSeparator,
     this.onEmptyDisplay,
-    this.isEmpty,
+    this.padding,
+    this.scrollDirection,
   });
+
+  final FutureOr<void> Function()? onRefresh;
+  final Future<LoadStatus> Function()? onLoading;
+
+  final int itemsCount;
+  final NullableIndexedWidgetBuilder itemBuilder;
+  final Widget Function(BuildContext context, int index)? itemSeparator;
+
+  final Widget? onEmptyDisplay;
+  final EdgeInsetsGeometry? padding;
+  final Axis? scrollDirection;
+  final bool initialRefresh;
+
+  @override
+  State<XListView> createState() => _XListViewState();
+}
+
+class _XListViewState extends State<XListView> {
+  late RefreshController rc;
+
+  @override
+  void initState() {
+    rc = RefreshController(initialRefresh: widget.initialRefresh);
+    super.initState();
+  }
+
+  void _onRefresh() async {
+    try {
+      await widget.onRefresh?.call();
+      rc.refreshCompleted();
+    } catch (e) {
+      rc.refreshFailed();
+    } finally {
+      rc.resetNoData();
+    }
+  }
+
+  void _onLoading() async {
+    final result = await widget.onLoading?.call();
+
+    switch (result) {
+      case LoadStatus.idle:
+        rc.loadComplete();
+      case LoadStatus.failed:
+        rc.loadFailed();
+      case LoadStatus.noMore:
+        rc.loadNoData();
+      default:
+        rc.loadComplete();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return SmartRefresher(
-      controller: refreshController,
-      enablePullUp: true,
-      onRefresh: onRefresh,
-      onLoading: onLoading,
-      header: const WaterDropMaterialHeader(),
-      child: (isEmpty ?? false)
+      controller: rc,
+      enablePullUp: widget.itemsCount != 0,
+      scrollDirection: widget.scrollDirection,
+      onRefresh: _onRefresh,
+      onLoading: _onLoading,
+      header: WaterDropMaterialHeader(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      child: (widget.itemsCount == 0)
           ? Container(
               alignment: Alignment.center,
-              margin: const EdgeInsets.only(bottom: kToolbarHeight),
+              margin: const EdgeInsets.only(bottom: kBottomNavigationBarHeight),
               child: SingleChildScrollView(
-                child: onEmptyDisplay ?? Container(),
+                child: widget.onEmptyDisplay ?? Container(),
               ),
             )
-          : ListView.builder(
-              padding: EdgeInsets.zero,
-              itemCount: itemsCount,
-              itemBuilder: itemBuilder,
-            ),
+          : (widget.itemSeparator != null)
+              ? ListView.separated(
+                  separatorBuilder: widget.itemSeparator!,
+                  padding: widget.padding ?? EdgeInsets.zero,
+                  itemCount: widget.itemsCount,
+                  itemBuilder: widget.itemBuilder,
+                )
+              : ListView.builder(
+                  padding: widget.padding ?? EdgeInsets.zero,
+                  itemCount: widget.itemsCount,
+                  itemBuilder: widget.itemBuilder,
+                ),
     );
   }
 }
